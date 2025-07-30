a22ebfb260c8a43089444157220a3d2e10d445b5
larkin <larkin.qubership@gmail.com>
2025-07-30 11:37:49 +0400

    fix: remove unnecessary @Component annotation

--- blue-green-state-monitor-spring/src/main/java/org/qubership/cloud/bluegreen/spring/log/BGStateConverterInitializer.java
+++ blue-green-state-monitor-spring/src/main/java/org/qubership/cloud/bluegreen/spring/log/BGStateConverterInitializer.java
@@ -3,13 +3,11 @@ package org.qubership.cloud.bluegreen.spring.log;
 import org.qubership.cloud.bluegreen.api.service.BlueGreenStatePublisher;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.stereotype.Component;
 
 /**
  * Initializes the {@link BGStateConverter} with a {@link BlueGreenStatePublisher} instance
  * once the Spring application context is fully refreshed.
  */
-@Component
 public class BGStateConverterInitializer implements ApplicationListener<ContextRefreshedEvent> {
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
